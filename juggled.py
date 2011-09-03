5ec12aa1924066502d297fa40ae15d1f95159c0b
Dan <dan@silvercrk.com>
2011-09-03 16:21:49 -0700

    added json

--- juggled.py
+++ juggled.py
@@ -2,11 +2,16 @@
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 
+import simplejson
+
 class Echo(DatagramProtocol):
 
     def datagramReceived(self, data, (host, port)):
         print "received %r from %s:%d" % (data, host, port)
+        event_data = simplejson.loads(data);
+        if event_data["type"] == "sensor_data":
+            print event_data["data"]["x"], event_data["data"]["y"], event_data["data"]["z"]
         self.transport.write(data, (host, port))
 
-reactor.listenUDP(9999, Echo())
+reactor.listenUDP(12345, Echo())
 reactor.run()
