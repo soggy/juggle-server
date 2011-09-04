df205b172f81316457b193c3cce88d206b6a6ccf
= <=>
2011-09-04 13:22:17 -0700

    Remove attempts to use matplotlib in server, commit changes for logger I should have included last time.

--- juggled.py
+++ juggled.py
@@ -1,37 +1,11 @@
 #!/usr/bin/env python
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor, task
-
 import simplejson
-
 import sys
-
 import numpy as np
-import matplotlib
-matplotlib.use('GTKAgg')
-
-import matplotlib.pyplot as plt
 
 import logger
-
-class Phone:
-    def __init__(self, server):
-        self.server = server
-        self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(111)
-        #self.line, = self.ax.plot(np.random.rand(10))
-        self.data = (np.ndarray(100), np.ndarray(100), np.ndarray(100))
-        self.line, = self.ax.plot(self.data[0])
-        self.ax.set_ylim(0, 1)
-
-    def adddata(self, d):
-        self.data[0][:-1] = self.data[0][1:]
-        self.data[0][-1] = d["data"]["x"]
-        
-    def update(self):
-        self.line.set_ydata(self.data[0])
-        self.fig.canvas.draw_idle()
-
 
 class Echo(DatagramProtocol):
     def __init__(self, log_file):
@@ -43,35 +17,23 @@ class Echo(DatagramProtocol):
         if log_file:        
             self.logger = logger.Logger(log_file);
 
-        self.loopingCall = task.LoopingCall(self.update)
-        self.loopingCall.start(1, False)
-
     def datagramReceived(self, data, (host, port)):
-        print "received %r from %s:%d" % (data, host, port)
-
-        if host not in self.phones.keys():
-            self.phones[host] = Phone(host)
-        phone = self.phones[host]
+        #print "received %r from %s:%d" % (data, host, port)
 
         if self.logger:
             self.logger.write(data);
             
-        event_data = simplejson.loads(data);		
+        event_data = simplejson.loads(data)
+        print event_data
         if event_data["type"] == "sensor_data":
             print event_data["data"]["x"], event_data["data"]["y"], event_data["data"]["z"]
-            phone.adddata(event_data)
-        #self.transport.write(data, (host, port))
-
-    def update(self):
-        for phone in self.phones.values():
-            phone.update()
-        return True # False terminates updates
 
 log_file = None;
 if len(sys.argv) >= 2:
     log_file = sys.argv[1];
 
-reactor.listenUDP(12345, Echo(log_file))
-plt.show()
+myecho = Echo(log_file)
+
+reactor.listenUDP(12345, myecho)
 reactor.run()
 
--- logger.py
+++ logger.py
@@ -3,30 +3,34 @@ import simplejson
 
 class Logger:
    def __init__(self, file_name):
-       self.log_file = open(file_name, "wb");
-       self.start_time = time.time();
-	   
+      self.log_file = open(file_name, "wb");
+      self.start_time = time.time();
+      
    def write(self, event):
-       log_event = {"time": time.time() - self.start_time, "event": event}
-       self.log_file.write("%s\n" % simplejson.dumps(log_event));
+      log_event = {"time": time.time() - self.start_time, "event": event}
+      self.log_file.write("%s\n" % simplejson.dumps(log_event));
 
 class LoggerSender:
-    def __init__(self, file_name):
-        self.log_file = open(file_name, "rb");
-        self.last_time = 0.0;
-            
-    def send_log(self, transport):
-        for line in self.log_file:
-            event = simplejson.loads(line);            
-            
-            # sleep for the differnce in time between the time in the current event, and the last_time
-            cur_time = float(event["time"])
-            if not self.last_time:
-                self.last_time = cur_time;
-                
-            time.sleep(cur_time - self.last_time);
-            
-            transport.write(event["event"])
-            
-            print event["time"], event["event"]
-        
+   def __init__(self, file_name):
+      self.log_file = open(file_name, "rb");
+      self.last_time = 0.0;
+
+   def nextevent(self):
+      line = self.log_file.readline()
+      if line is None:
+         return None
+      event = simplejson.loads(line)
+      cur_time = float(event["time"])
+      if not self.last_time:
+         self.last_time = cur_time
+      time.sleep(cur_time - self.last_time)
+      #print event["time"], event["event"]
+      self.last_time = cur_time
+      return event["event"]
+   
+   def send_log(self, transport):
+      while True:
+         event = self.nextevent()
+         if event is None:
+            break
+         transport.write(event)
