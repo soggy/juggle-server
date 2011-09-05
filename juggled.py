6c8041e63cedf14199fc7138b64b1b50391d6079
Dan <gromph99@gmail.com>
2011-09-04 18:47:54 -0700

    added TcpEcho

--- juggled.py
+++ juggled.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from twisted.internet.protocol import DatagramProtocol
-from twisted.internet import reactor, task
+from twisted.internet import reactor, task, protocol
 import simplejson
 import sys
 
@@ -31,6 +31,15 @@ class Echo(DatagramProtocol):
         #if event_data["type"] == "sensor_data":
         #    print event_data["data"]["x"], event_data["data"]["y"], event_data["data"]["z"]
 
+class TcpEcho(protocol.Protocol):
+    """This is just about the simplest possible protocol"""
+    
+    def dataReceived(self, data):
+        "As soon as any data is received, write it back."
+        self.transport.write("FROM JUGGLED\n")
+        print data
+        
+        
 log_file = None;
 if len(sys.argv) >= 2:
     log_file = sys.argv[1];
@@ -38,5 +47,11 @@ if len(sys.argv) >= 2:
 myecho = Echo(log_file)
 
 reactor.listenUDP(12345, myecho)
+
+# listen to tcp
+factory = protocol.ServerFactory()
+factory.protocol = TcpEcho
+reactor.listenTCP(12345,factory)
+
 reactor.run()
 
